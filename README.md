1c5f21d44f6b0534b1b2f42dd4b146a6ab38d7db
peter279k <peter279k@gmail.com>
2021-12-20 17:21:53 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Hardware Monitor for Linux-like operating system
+
+## Usage
+
+- Download `monitor.sh`
+- Running `chmod 755 ./monitor.sh` to make this Bash script executable
+- Running `./monitor.sh` or setting Cron job with specific time to execute this Bash script
+- Done
+
+## References
+
+- https://helloacm.com/bash-script-to-monitor-the-cpu-frequency-and-temperature-on-raspberry-pi
+- https://www.2daygeek.com/linux-shell-script-to-monitor-cpu-memory-swap-usage-send-email
---
+++ monitor.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "Monitoring CPU temperature and frequency is starting..."
+echo "-------------------------------------------------------"
+
+cpu_temp=$(head -n 1 /sys/class/thermal/thermal_zone0/temp)
+cpu_temp=$(echo "scale=2; $cpu_temp / 1000" | bc -l)
+
+cpu_frequency=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq)
+cpu_frequency=$(echo "scale=2; $cpu_frequency / 1000" | bc -l)
+
+echo "Monitoring Memory and swap usage is starting..."
+echo "-------------------------------------------------------"
+
+smem=$(free | grep Mem | awk '{print $3/$2*100}')
+smem=$(echo "scale=2; $smem / 1.0" | bc -l)
+
+sswap=$(free | grep Swap | awk '{print $3/$2*100}')
+sswap=$(echo "scale=2; $sswap / 1.0" | bc -l)
+
+echo "Monitoring disk usage is starting..."
+echo "-------------------------------------------------------"
+
+disk_usage=$(df -lh / | tail -n 1 | awk '{print $5}' | sed -e "s/%//g")
+file_name="$(date --iso-8601=seconds).txt"
+
+echo "$cpu_temp" >> $file_name
+echo "$cpu_frequency" >> $file_name
+echo "$smem" >> $file_name
+echo "$sswap" >> $file_name
+echo "$disk_usage" >> $file_name
