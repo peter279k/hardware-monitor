88323c9acb0c34eb8a05f9e15b0bd5c206351868
peter279k <peter279k@gmail.com>
2022-01-07 13:00:45 +0800

    Add csv_importer_mysql.sh and related sample auth

--- README.md
+++ README.md
@@ -2,20 +2,26 @@
 
 ## Usage
 
-- Download `monitor.sh`
-- Running `chmod 755 ./monitor.sh` to make this Bash script executable
-- Running `./monitor.sh` or setting Cron job with specific time to execute this Bash script
-- Done
+- Download the `monitor.sh`.
+- Running the `chmod 755 ./monitor.sh` command to make this Bash script executable.
+- Running the `./monitor.sh` or setting Cron job with specific time to execute this Bash script.
+- Done.
 
 ## Data converter
 
-- Download `csv_data_converter.sh` to convert all text to CSV files in current directory.
-- Download `csv_data_converter_by_day.sh` to convert yesterday text to CSV files in current directory.
+- Download the `csv_data_converter.sh` to convert all text to CSV files in current directory.
+- Download the `csv_data_converter_by_day.sh` to convert yesterday text to CSV files in current directory.
 - All of the above Bash scripts will let converted files be saved as `converted` directory.
+
+## Data importer with mysql command
+
+- WIP.
+- Create the ``
+- Download the `` to let all CSV files be imported
 
 ## Data cleaner
 
-- Download `data_cleaner_by_day.sh` to remove yesterday text file in current directory.
+- Download the `data_cleaner_by_day.sh` to remove yesterday text file in current directory.
 
 ## References
 
---
+++ csv_importer_mysql.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+green_color='\e[0;32m'
+red_color='\e[0;31m'
+yellow_color='\e[0;33m'
+rest_color='\e[0m'
+
+echo -e $green_color"Data importer has been started!"$rest_color
+echo -e $yellow_color"Checking mysql command is available"$rest_color
+echo ""
+
+which mysql 2>&1 > /dev/null
+
+if [[ $? != 0 ]]; then
+    echo -e $red_color"mysql command not found"$rest_color
+    exit 1;
+fi;
+
+if [[ ! -f ./mysql_auth.txt ]]; then
+    echo -e $red_color"Please specify ./mysql_auth.txt file to create MySQL DB authentication"$rest_color
+    exit 1;
+fi;
+
+mysql_auth=$(cat ./mysql_auth.txt)
+
+ip_address=$(echo $mysql_auth | awk '{print $1}')
+user_name=$(echo $mysql_auth | awk '{print $2}')
+port_number=$(echo $mysql_auth | awk '{print $3}')
+password=$(echo $mysql_auth | awk '{print $4}')
+db_name=$(echo $mysql_auth | awk '{print $5}')
+table_name=$(echo $mysql_auth | awk '{print $6}')
+
+for csv_file_name in $(ls ./converted/*.csv)
+do
+    insert_cmd="INSERT INTO $db_name.$table_name FORMAT CSV $(cat ./converted/$csv_file_name)"
+    mysql -h "$ip_address" -u "$user_name" -P "$port_number" -p"$password" -e "$insert_cmd"
+    if [[ $? != 0 ]]; then
+        echo -e $red_color"The $csv_file_name is failed to import."$rest_color
+    fi;
+done;
+
+echo -e $green_color"Data importer has been done!"$rest_color
