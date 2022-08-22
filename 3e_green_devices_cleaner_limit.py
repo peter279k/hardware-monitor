201346cd0e2e8304d0d176d4ed7d28e0735d001b
peter279k <peter279k@gmail.com>
2022-08-22 10:23:46 +0800

    Add batch_clean.sh and 3e_green_devices_cleaner_limit

--- 3e_green_devices_cleaner_limit.py
+++ 3e_green_devices_cleaner_limit.py
@@ -12,12 +12,16 @@ if os.path.isdir(archived_path) is False:
 
 csv_file_paths = glob.glob(archived_path + '*.csv')
 csv_file_paths.extend(glob.glob('./*.csv'))
+counter = 0
 for csv_file_path in csv_file_paths:
     file_date = datetime.datetime.fromtimestamp(int(csv_file_path.split('_')[1][0:-4]) / 1000)
     diff_days = (today_date - file_date).days
     if diff_days > 7:
         os.remove(csv_file_path)
         print(csv_file_path + ' is removed.')
+        counter += 1
+    if counter == 100:
+        break
 
-print('Cleaner has been done.')
+print('Cleaner has been done. (%s)' % counter)
 
---
+++ batch_clean.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+for value in $(seq 1 10000); do python3 3e_green_devices_cleaner_limit.py sleep 5; done;
