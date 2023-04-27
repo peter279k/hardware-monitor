b5060039859e0c3ff37cfc9f4eec809b91b7ed0a
peter279k <peter279k@gmail.com>
2023-04-27 16:34:57 +0800

    date_range setting & ignore panasonic sensor dir

--- 3e_green_cloud_panasonic.py
+++ 3e_green_cloud_panasonic.py
@@ -7,8 +7,6 @@ api_setting_path = './uuid_3egreen_panasonic_url.txt'
 uuid_path = './uuid_3egreen_panasonic.txt'
 headers = {'Content-Type': 'application/json'}
 
-date_range = '2023-04-18_2023-04-27'
-
 sensor_data_dir = './panasonic_sensor_demo_data'
 
 if os.path.isdir(sensor_data_dir) is False:
@@ -26,17 +24,19 @@ handler = open(api_setting_path, 'r')
 contents = handler.readlines()
 handler.close()
 
-if len(contents) != 1:
+if len(contents) != 2:
     print('The ' + api_setting_path + ' format is invalid.')
     exit(1)
 
 history_api_url = contents[0][0:-1]
+date_range = contents[1][0:-1]
 
 handler = open(uuid_path, 'r')
 contents = handler.readlines()
 handler.close()
 
 for uuid_mac_address in contents:
+    sensor_path = sensor_data_dir + '/' + uuid_mac_address[0:-1] + '_' + date_range + '.json'
     try:
         post_data = {
             'macaddress': uuid_mac_address[0:-1],
@@ -47,10 +47,9 @@ for uuid_mac_address in contents:
 
     except Exception as e:
         print(str(e))
+        print(sensor_path + ' is failed to be saved.')
         continue
 
-
-    sensor_path = sensor_data_dir + '/' + uuid_mac_address[0:-1] + '_' + date_range
     handler = open(sensor_path, 'w')
     handler.write(response.text)
     handler.close()
