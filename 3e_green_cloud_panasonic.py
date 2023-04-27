ccf1e5ad2913ab312b597f06ad9fd344a790418c
peter279k <peter279k@gmail.com>
2023-04-27 16:27:07 +0800

    Let API URL be configurable

--- 3e_green_cloud_panasonic.py
+++ 3e_green_cloud_panasonic.py
@@ -3,19 +3,34 @@ import json
 import requests
 
 
-history_api_url = 'http://3egreenserverapi.ddns.net:9001/api/data/clamp/device/historical/minute'
+api_setting_path = './uuid_3egreen_panasonic_url.txt'
 uuid_path = './uuid_3egreen_panasonic.txt'
 headers = {'Content-Type': 'application/json'}
 
 date_range = '2023-04-18_2023-04-27'
 
 sensor_data_dir = './panasonic_sensor_demo_data'
-if os.path.isdir() is False:
+
+if os.path.isdir(sensor_data_dir) is False:
     os.mkdir(sensor_data_dir)
+
+if os.path.isfile(api_setting_path) is False:
+    print('The API setting file is not found.')
+    exit(1)
 
 if os.path.isfile(uuid_path) is False:
     print('The UUID file is not found.')
     exit(1)
+
+handler = open(api_setting_path, 'r')
+contents = handler.readlines()
+handler.close()
+
+if len(contents) != 1:
+    print('The ' + api_setting_path + ' format is invalid.')
+    exit(1)
+
+history_api_url = contents[0][0:-1]
 
 handler = open(uuid_path, 'r')
 contents = handler.readlines()
@@ -32,6 +47,7 @@ for uuid_mac_address in contents:
 
     except Exception as e:
         print(str(e))
+        continue
 
 
     sensor_path = sensor_data_dir + '/' + uuid_mac_address[0:-1] + '_' + date_range
